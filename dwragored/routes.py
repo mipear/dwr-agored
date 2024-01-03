2e6c6ee0b6f3a7a196f3f561e0334aa5a5315a66
mipear <>
2024-01-03 15:15:51 +0000

    Added Location and abilitity for users to add location

--- dwragored/routes.py
+++ dwragored/routes.py
@@ -1,8 +1,23 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from dwragored import app, db
-from dwragored.models import Location, Experience
+from dwragored.models import Location, My Swim
 
 
 @app.route("/")
 def home():
     return render_template("myswim.html")
+
+
+@app.route("/location")
+def location():
+    return render_template("location.html")
+
+
+@app.route("/add_location", methods=["GET", "POST"])
+def add_location():
+    if request.method == "POST":
+        location = Location(location_name=request.form.get("location_name"))
+        db.session.add(location)
+        db.session.commit()
+        return redirect(url_for("location"))
+    return render_template("add_location.html")
