c50cf3d09895efaee86f6e60b4f3d326832c0f16
mipear <>
2024-01-05 18:27:43 +0000

    Added Edit functionality to locations

--- dwragored/routes.py
+++ dwragored/routes.py
@@ -22,3 +22,12 @@ def add_location():
         db.session.commit()
         return redirect(url_for("location"))
     return render_template("add_location.html")
+
+@app.route("/edit_location/<int:location_id>", methods=["GET", "POST"])
+def edit_location(location_id):
+    location = Location.query.get_or_404(location_id)
+    if request.method == "POST":
+        location.location_name = request.form.get("location_name")
+        db.session.commit()
+        return redirect(url_for("location"))
+    return render_template("edit_location.html", location=location)
